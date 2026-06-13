0df6f2ff9746a1268338a5a8c128082deb76c833
Lindsey Bowen <greyjediak@gmail.com>
2026-06-12 22:12:49 -0500

    Disp tested

--- GeeberToy/src/main.cpp
+++ GeeberToy/src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DISP_CS    D0
+#define DISP_RES   D1
+#define DISP_DC    D2
+
+#define DISP_SDA   D10   // MOSI
+#define DISP_SCL   D8    // SCK
+
+Adafruit_ST7789 disp = Adafruit_ST7789(
+  DISP_CS,
+  DISP_DC,
+  DISP_SDA,
+  DISP_SCL,
+  DISP_RES
+);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Init display");
+
+  disp.init(240, 240);
+  disp.setRotation(0);
+  disp.fillScreen(ST77XX_RED);
+
+  Serial.println("Screen should be red");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
